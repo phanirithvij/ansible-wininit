2cbc12c79105e880809f566bf84a0731254929a9
phanirithvij <phanirithvij2000@gmail.com>
2023-06-21 23:02:55 +0530

    enable and update wsl init.ps1

--- init.ps1
+++ init.ps1
@@ -1,4 +1,33 @@
-# todo get the alpine linux from outside microsoft store
+# Check if the current user has administrative privileges
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+if (-not $isAdmin) {
+	$scriptPath = [System.Diagnostics.Process]::GetCurrentProcess().MainModule.FileName
+    # Relaunch the script with administrative privileges
+    Start-Process -FilePath $scriptPath -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"" -Verb RunAs
+    exit
+}
+
+# Check if WSL is installed
+$wslFeature = Get-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -ErrorAction SilentlyContinue
+
+if ($wslFeature -eq $null) {
+    # WSL feature not installed, enable it
+    $enableResult = Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online
+    # Check if a restart is needed
+    if ($enableResult.RestartNeeded) {
+        Write-Output "WSL feature has been enabled. Please restart your computer for the changes to take effect."
+        exit
+    }
+    else {
+        Write-Output "WSL feature has been enabled. Updating to wsl version 2."
+		wsl --set-default-version 2
+    }
+} else {
+    Write-Output "WSL is already installed. Updating to wsl version 2."
+	wsl --set-default-version 2
+}
+
+# get the alpine linux from outside microsoft store
 
 if (-Not (Test-Path "Alpine.zip")) {
 	irm "https://github.com/phanirithvij/ansible-wininit/releases/download/v0.0.1-alpha/Alpine.zip" -outfile Alpine.zip
--- scripts/build_distro.sh
+++ scripts/build_distro.sh
@@ -4,10 +4,13 @@ apk add make curl libarchive-tools sudo git
 
 git clone https://github.com/yuk7/AlpineWSL
 
+patchver="3.18.2"
+
 cd AlpineWSL
-git apply ../alpine-3.18.2.patch
+git apply ../alpine-$patchver.patch
 make -j8
 mv Alpine.zip ..
+echo "Alpine.zip now has alpine wsl distro $patchver"
 cd ..
 
 rm -rf AlpineWSL
