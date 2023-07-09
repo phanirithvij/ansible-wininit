29648b0d7a7761302d05d50be89a77c82e840af1
phanirithvij <phanirithvij2000@gmail.com>
2023-07-10 00:49:36 +0530

    todo

--- wsl/wsl2/scripts/init.sh
+++ wsl/wsl2/scripts/init.sh
@@ -28,18 +28,19 @@ EOF
 # This has a libyaml issue which is requiring a source build
 # And that build requires a shit ton of time and so far hasn't succeded
 # Installing alpine package for now
-# Also ansible is too slow
-#apk add python3 py3-pip
-#pip install pipx
-#pipx install --system-site-packages --include-deps ansible
+# Also ansible is too slow try with mitogen or rust rewrite incorporating mitogen principles (herculean task)
+# apk add python3 py3-pip
+# pip install pipx
+# pipx install --system-site-packages --include-deps ansible
 # TODO temp downgrade for mitogen
 apk add ansible
 # apk add ansible-core=2.13.6-r0 ansible=6.6.0-r0 --repository=http://dl-cdn.alpinelinux.org/alpine/v3.17/community
+mkdir -p ~/.cache
 echo $(which ansible) --version
 $(which ansible) --version
 mkdir -p /etc/ansible
 cp ../../ansible/ansible.cfg /etc/ansible/ansible.cfg
-ansible-galaxy collection install community.general
+# ansible-galaxy collection install community.general
 apk add py3-pip
 pip install mitogen
 apk del py3-pip
@@ -59,20 +60,3 @@ apk del py3-pip
 echo "tmuxp load -d ${PWD}/scripts/ssh_t.yml" >> ~/.bashrc
 # run sshd with tmux
 source ~/.bashrc
-
-# Optional tools which are to be made into ansible notebooks or roles or whatnot
-
-# lf setup (TODO copy config file from github yadm/chezmoi/dotdrop/...)
-apk add lf
-mkdir -p ~/.config/lf
-echo "set hidden!" > ~/.config/lf/lfrc
-
-# https://github.com/denisidoro/navi/blob/master/docs/config_file.md
-# navi setup (default config comes with skim, zsh, overwrite it with the example one which is fzf, bash)
-apk add fzf navi
-navi info config-example > $(navi info config-path)
-echo "navi widget bash > /tmp/navi_widget.sh" >> ~/.bashrc
-echo "source /tmp/navi_widget.sh" >> ~/.bashrc
-
-# TODO fish shell
-#apk add fish # this is auto installing navi, lf, fzf fish plugins
