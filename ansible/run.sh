ba98a3bf62f09dcd5fa4afa0a8583504d5d5f1b8
phanirithvij <phanirithvij2000@gmail.com>
2023-07-10 00:49:08 +0530

    ansible lint ignore name case

--- ansible/run.sh
+++ ansible/run.sh
@@ -2,7 +2,7 @@ set -x
 
 #ANSIBLE_KEEP_REMOTE_FILES=True \ # <-- disables pipelining which slows by 2x but useful for understanding ansible internals
 
-#ANSIBLE_CONFIG=/etc/ansible/ansible.cfg \
+ANSIBLE_CONFIG=./ansible.cfg \
 ansible-playbook \
     -i inventory.ini \
     local-playbook.yml $@
