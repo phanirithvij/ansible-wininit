cb8ee419ad32d9c2e0378d9d44050ecbd4b3b525
phanirithvij <phanirithvij2000@gmail.com>
2023-07-18 20:23:22 +0530

    minor

--- ansible/scripts/profile.sh
+++ ansible/scripts/profile.sh
@@ -1,7 +1,5 @@
 set -x
 
-#ANSIBLE_KEEP_REMOTE_FILES=True \ # <-- disables pipelining which slows by 2x but useful for understanding ansible internals
-
 ANSIBLE_CONFIG=conf/ansible.profile.cfg \
 ansible-playbook \
     -i inv/inv.ini \
